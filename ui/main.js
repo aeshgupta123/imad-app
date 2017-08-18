faee42cdf50459133759796baea9346cdee92106
aeshgupta123 <aeshgupta123@gmail.com>
2017-08-19 04:06:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
+//change the text of the main-text
 var element=document.getElementById("main-text");
 element.innerHTML='Hi!I\'m Aesh Gupta,This is my first Web App.';
+
+var img=document.getElementbyId("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+5;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+};
